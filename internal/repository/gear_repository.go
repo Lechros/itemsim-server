67a5aea5aa423f0f8df9fc1450078e0560ad129c
Lechros <0onionion0@gmail.com>
2024-12-23 13:00:16 +0900

    feat: optimize search by batch regex

--- internal/repository/gear_repository.go
+++ internal/repository/gear_repository.go
@@ -9,131 +9,90 @@ import (
 	"slices"
 	"strconv"
 	"strings"
-	"unicode/utf8"
 )
 
-var gears map[string]json.RawMessage
-var names map[string]string
-var gearOrigins map[string][2]int
-var gearRawOrigins map[string][2]int
-var invertedIndex util.HangulInvertedIndex[string]
+var gears map[int]json.RawMessage
+var names map[int]string
+var gearOrigins map[int][2]int
+var gearRawOrigins map[int][2]int
+var concatNames string
+var concatIndex []int
+var concatIds []int
 
 func SearchGearByName(search string, size int) []json.RawMessage {
 	search = strings.ToLower(search)
 	pattern, _ := hangul_regexp.GetPattern(search, false, true, true)
 	regex := rure.MustCompile("(?i)" + pattern) // (?i): Case insensitive
 
-	exactIds := make([]string, 0, 10)
-	regexIds := make([]string, 0, 10)
+	// Can't infer capacity from FindAll's length since single line can match multiple times.
+	matchedIds := make([]int, 0, 10)
 
-	useIndex := false
-	if utf8.RuneCountInString(search) > 1 {
-		foundIds := invertedIndex.FindAll(search)
-		if foundIds != nil {
-			useIndex = true
-			for _, id := range foundIds {
-				name := names[id]
-				if strings.Contains(name, search) {
-					exactIds = append(exactIds, id)
-				} else if len(exactIds) < size && regex.IsMatch(name) {
-					regexIds = append(regexIds, id)
-				}
+	matches := regex.FindAll(concatNames)
+	lastIndex := -1
+	for i, match := range matches {
+		if i%2 == 0 {
+			index, found := slices.BinarySearch(concatIndex, match)
+			if !found {
+				index--
 			}
-		}
-	}
-	if !useIndex {
-		for id, name := range names {
-			if strings.Contains(name, search) {
-				exactIds = append(exactIds, id)
-			} else if len(exactIds) < size && regex.IsMatch(name) {
-				regexIds = append(regexIds, id)
+			if index > lastIndex {
+				id := concatIds[index]
+				matchedIds = append(matchedIds, id)
+				lastIndex = index
 			}
 		}
 	}
 
-	if len(exactIds) >= size {
-		sortExactMatches(exactIds, search)
+	sortRegexMatches(matchedIds, regex)
 
-		result := make([]json.RawMessage, size)
-		for i, id := range exactIds[:size] {
-			result[i] = gears[id]
+	resultSize := min(len(matchedIds), size)
+	result := make([]json.RawMessage, resultSize)
+	for i, id := range matchedIds {
+		if len(result) == resultSize {
+			break
 		}
-		return result
-	} else {
-		sortExactMatches(exactIds, search)
-		sortRegexMatches(regexIds, regex)
-
-		resultSize := min(len(exactIds)+len(regexIds), size)
-		result := make([]json.RawMessage, 0, resultSize)
-		for _, id := range exactIds {
-			result = append(result, gears[id])
-		}
-		for _, id := range regexIds {
-			if len(result) == resultSize {
-				break
-			}
-			result = append(result, gears[id])
-		}
-		return result
+		result[i] = gears[id]
 	}
+	return result
 }
 
 func GetGearById(id string) (json.RawMessage, bool) {
-	gear, ok := gears[id]
+	intId, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, false
+	}
+	gear, ok := gears[intId]
 	return gear, ok
 }
 
 func GetGearIconOriginById(id string) ([2]int, bool) {
-	origin, ok := gearOrigins[id]
+	intId, err := strconv.Atoi(id)
+	if err != nil {
+		return [2]int{}, false
+	}
+	origin, ok := gearOrigins[intId]
 	return origin, ok
 }
 
 func GetGearRawIconOriginById(id string) ([2]int, bool) {
-	origin, ok := gearRawOrigins[id]
+	intId, err := strconv.Atoi(id)
+	if err != nil {
+		return [2]int{}, false
+	}
+	origin, ok := gearRawOrigins[intId]
 	return origin, ok
 }
 
-func sortExactMatches(exactIds []string, search string) {
+func sortRegexMatches(ids []int, regex *rure.Regex) {
 	type IdInfo struct {
-		intId   int
-		nameLen int
-		index   int
-	}
-	infos := make([]IdInfo, len(exactIds))
-	for i, id := range exactIds {
-		intId, _ := strconv.Atoi(id)
-		name := names[id]
-		index := strings.Index(name, search)
-		infos[i] = IdInfo{intId, len(name), index}
-	}
-	slices.SortFunc(infos, func(aInfo, bInfo IdInfo) int {
-		// Contains substring at different index
-		if aInfo.index != bInfo.index {
-			return aInfo.index - bInfo.index
-		}
-		// Contains substring at same index
-		// Shorter name should be more exact match
-		if aInfo.nameLen != bInfo.nameLen {
-			return aInfo.nameLen - bInfo.nameLen
-		}
-		return aInfo.intId - bInfo.intId
-	})
-	for i, info := range infos {
-		exactIds[i] = strconv.Itoa(info.intId)
-	}
-}
-
-func sortRegexMatches(regexIds []string, regex *rure.Regex) {
-	type IdInfo struct {
-		intId int
+		id    int
 		index [2]int
 	}
-	infos := make([]IdInfo, len(regexIds))
-	for i, id := range regexIds {
-		intId, _ := strconv.Atoi(id)
+	infos := make([]IdInfo, len(ids))
+	for i, id := range ids {
 		name := names[id]
 		start, end, _ := regex.Find(name)
-		infos[i] = IdInfo{intId, [2]int{start, end}}
+		infos[i] = IdInfo{id, [2]int{start, end}}
 	}
 	slices.SortFunc(infos, func(aInfo, bInfo IdInfo) int {
 		if aInfo.index[0] != bInfo.index[0] {
@@ -142,10 +101,10 @@ func sortRegexMatches(regexIds []string, regex *rure.Regex) {
 		if aInfo.index[1] != bInfo.index[1] {
 			return aInfo.index[1] - bInfo.index[1]
 		}
-		return aInfo.intId - bInfo.intId
+		return aInfo.id - bInfo.id
 	})
 	for i, info := range infos {
-		regexIds[i] = strconv.Itoa(info.intId)
+		ids[i] = info.id
 	}
 }
 
@@ -154,9 +113,10 @@ func init() {
 	util.ReadJson("resources/gear-origin.json", &gearOrigins)
 	util.ReadJson("resources/gear-raw-origin.json", &gearRawOrigins)
 
-	names = make(map[string]string)
-	invertedIndex = util.HangulInvertedIndex[string]{}
-	invertedIndex.Init()
+	names = make(map[int]string)
+	concatIndex = make([]int, 0, len(gears))
+	concatIds = make([]int, 0, len(gears))
+	builder := strings.Builder{}
 	for id, gearData := range gears {
 		var gear map[string]interface{}
 		if err := json.Unmarshal(gearData, &gear); err != nil {
@@ -164,6 +124,11 @@ func init() {
 		}
 		name := strings.ToLower(gear["name"].(string))
 		names[id] = name
-		invertedIndex.Add(id, name)
+
+		concatIndex = append(concatIndex, builder.Len())
+		concatIds = append(concatIds, id)
+		builder.WriteString(name)
+		builder.WriteRune('\n')
 	}
+	concatNames = builder.String()
 }
